c7808324abb1237e312bcfa778924ac9a12a7567
Rianna Winston <riannawinston@macbook-air.myfiosgateway.com>
2023-07-08 17:55:21 -0400

    first commit

---
+++ apartments.js
@@ -0,0 +1,30 @@
+const apartments = [
+    {
+      sqFt: 800,
+      price: 1200,
+      address: '123 Main Street',
+      isPetFriendly: true,
+      waterfrontView: false,
+      amenities: ['gym', 'pool'],
+      nearbyPlaces: ['park', 'grocery store', 'restaurant']
+    },
+    {
+      sqFt: 1000,
+      price: 1500,
+      address: '456 Elm Avenue',
+      isPetFriendly: true,
+      waterfrontView: true,
+      amenities: ['gym', 'pool', 'playground'],
+      nearbyPlaces: ['beach', 'shopping mall', 'movie theater']
+    },
+    {
+      sqFt: 1200,
+      price: 1800,
+      address: '789 Oak Lane',
+      isPetFriendly: false,
+      waterfrontView: false,
+      amenities: ['gym', 'laundry'],
+      nearbyPlaces: ['library', 'coffee shop', 'park']
+    }
+  ];
+  
